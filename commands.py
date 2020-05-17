1040f586fa58b787b4504ab93ac1c93de10ac709
Daniel Soto Jaimes <dsotojaimes@gmail.com>
2020-05-16 21:21:32 -0500

    More modular change to start list

--- commands.py
+++ commands.py
@@ -1,22 +1,20 @@
 import matplotlib.pyplot as plt
-
 from processing.start_scrapper import star_scrapper as str_sc
 
 st = str_sc()
-dot_map = st.create_dot_map()
+dot_map = st.create_all_stars_map()
 
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text='as')
+    context.bot.send_message(chat_id=update.effective_chat.id, text='Hola Cata ;D')
 
 
 def showPlot(update, context):
     x = dot_map[0]
     y = dot_map[1]
     ax = plt.gca()
-    ax.scatter(x, y, s=1.0, c="black", )
     ax.axes.xaxis.set_visible(False)
     ax.axes.yaxis.set_visible(False)
-    plt.scatter(x, y)
+    ax.scatter(x, y, s=1.0, c="black")
     plt.axis('off')
     plt.savefig('figure.png')
     context.bot.send_photo(chat_id=update.effective_chat.id, photo = open('./figure.png', 'rb'))
--- processing/start_scrapper.py
+++ processing/start_scrapper.py
@@ -4,23 +4,24 @@ class star_scrapper:
         self.stars_list = self.create_stars_list()
 
     def coords_to_pixel(self, orig_x, orig_y, size):
-        # todo el centro de la imagen y añado el correspondiente offset
         x_pos = size / 2 + (orig_x * size / 2)
         y_pos = size / 2 - (orig_y * size / 2)
         return (x_pos, y_pos)
 
-    def create_dot_map(self):
+    def create_all_stars_map(self):
         x = []
         y = []
+
         for linea in  self.stars_list:
-            lineaP = linea.split(' ')
-            print(lineaP)
-            new_sys = self.coords_to_pixel(float(lineaP[0]), float(lineaP[1]), self.SIZE)
+            new_sys = self.coords_to_pixel(float(linea[0]), float(linea[1]), self.SIZE)
             x.append(new_sys[0])
             y.append(new_sys[1])
         return (x, y)
 
     def create_stars_list(self):
         with open('C:/Users/Daniel/PycharmProjects/bot_constellations/processing/data/stars.txt', 'r') as f:
-            return f.read().splitlines()
+            str_arr = []
+            for  linea in  f.read().splitlines():
+                str_arr.append(linea.split(' '))
+            return str_arr
 
